ae2a2b88e51e7b3d494950905eb49d587ffa12f8
Andrei <andrei.25479@gmail.com>
2022-10-06 19:29:03 +0300

    Default

---
+++ ClubMembership/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClubMembership.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
